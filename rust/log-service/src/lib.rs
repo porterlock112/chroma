491d59e27e96c4c11f8906df2632274c7482bc39
Macronova <60079945+Sicheng-Pan@users.noreply.github.com>
2025-05-30 20:16:17 -0500

    Cherry-pick with conflicts: fc943af663bda4fe961378869a9d22adc3191653

--- rust/log-service/src/lib.rs
+++ rust/log-service/src/lib.rs
@@ -1219,16 +1219,20 @@ impl LogService for LogServer {
             let witness = cursors.load(cursor_name).await.map_err(|err| {
                 Status::new(err.code().into(), format!("Failed to load cursor: {}", err))
             })?;
-            // This is the existing compaction_offset, which is the last record that was compacted.
+            // This is the existing compaction_offset, which is the next record to compact.
             let offset = witness
                 .map(|x| x.1.position)
+<<<<<<< HEAD
                 .unwrap_or(LogPosition::from_offset(0));
+=======
+                .unwrap_or(LogPosition::from_offset(1));
+            tracing::event!(Level::INFO, offset = ?offset);
+>>>>>>> fc943af66 ([BUG] Rust log service fork offsets off by one (#4689))
             wal3::copy(
                 &storage,
                 &options,
                 &log_reader,
-                // + 1 to get to the first uncompacted record.
-                offset + 1u64,
+                offset,
                 target_prefix.clone(),
             )
             .await
@@ -1251,7 +1255,9 @@ impl LogService for LogServer {
                 ));
             }
             Ok(Response::new(ForkLogsResponse {
-                compaction_offset: offset.offset(),
+                // NOTE: The upstream service expects the last compacted offset as compaction offset
+                compaction_offset: (offset - 1u64).offset(),
+                // NOTE: The upstream service expects the last uncompacted offset as enumeration offset
                 enumeration_offset: (max_offset - 1u64).offset(),
             }))
         }
